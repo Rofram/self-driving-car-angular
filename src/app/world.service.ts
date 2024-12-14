d6a8bfc72dd4b6c34c9c2756192e6da68beb7b40
Rodrigo de França <howdrigo.fr@gmail.com>
2024-12-14 20:52:39 -0300

    implement draw road boundaries and road lane draw

--- src/app/world.service.ts
+++ src/app/world.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { Graph } from './math/graph';
 import { Envelop } from './primitives/envelop';
 import { Drawable } from './interfaces/drawable.interface';
+import {Polygon} from "./primitives/polygon";
+import {Segment} from "./primitives/segment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorldService implements Drawable {
+  canvasCtx!: CanvasRenderingContext2D;
   graph!: Graph;
   roadWidth: number = 100;
-  roundness: number = 3;
-  envelop: Envelop[] = [];
-  canvasCtx!: CanvasRenderingContext2D;
+  roundness: number = 10;
+  envelops: Envelop[] = [];
+  roadBoundaries: Segment[] = [];
 
   constructor() { }
 
@@ -36,19 +39,28 @@ export class WorldService implements Drawable {
   }
 
   generate() {
-    this.envelop.length = 0;
+    this.envelops.length = 0;
     for (const segment of this.graph.segments) {
-      this.envelop.push(new Envelop(segment, this.roadWidth, this.roundness));
+      this.envelops.push(new Envelop(segment, this.roadWidth, this.roundness));
     }
+
+    this.roadBoundaries = Polygon.union(this.envelops.map(envelop => envelop.polygon));
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    for (const envelop of this.envelop) {
-      envelop.draw(ctx);
+    for (const road of this.envelops) {
+      road.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
+    }
+    for (const streetLane of this.graph.segments) {
+      streetLane.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
+    }
+    for (const roadBoundary of this.roadBoundaries) {
+      roadBoundary.draw(ctx, {color: "white", width: 4});
     }
   }
 
   display() {
+    this.generate();
     this.draw(this.canvasCtx);
   }
 }
